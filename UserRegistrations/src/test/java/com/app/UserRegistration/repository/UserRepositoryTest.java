725256cb15e47140ea8f15f37ccc314c301b9d95
Nikhil Palve <81505833+nikhilpalve18@users.noreply.github.com>
2024-03-27 09:22:32 +0530

    Add unit test cases using junit and mockito

--- UserRegistrations/src/test/java/com/app/UserRegistration/repository/UserRepositoryTest.java
+++ UserRegistrations/src/test/java/com/app/UserRegistration/repository/UserRepositoryTest.java
@@ -2,6 +2,8 @@ package com.app.UserRegistration.repository;
 
 import com.app.UserRegistration.entity.Contact;
 import com.app.UserRegistration.entity.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,48 +20,43 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    void itShouldfindByUsername() {
-        // given
-        User user = buildUserObject();
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        user = buildUserObject();
         userRepository.save(user);
-
-        // when
-        User userResult = userRepository.findByUsername("nikhilpalve18");
-
-        assertThat(user).isEqualTo(userResult);
     }
 
-    @Test
-    void itShouldCheckUsernameDoesNotExists() {
-        // given
-        String username = "nikhilpalve18";
-        // User user = buildUserObject();
-        // userRepository.save(user);
-
-        // when
-        User expected = userRepository.findByUsername(username);
-
-        assertThat(expected).isNull();
+    @AfterEach
+    void tearDown() {
+        user = null;
+        userRepository.deleteAll();
     }
-    private static User buildUserObject(){
-        Contact contact = Contact.builder()
-                .email("nikhilpalve18@gmail.com")
-                .mobile("9087676565")
-                .build();
+
+    private static User buildUserObject() {
+        Contact contact = Contact.builder().email("nikhilpalve18@gmail.com").
+                mobile("9087676565").build();
 
         // Create a list of contacts
         List<Contact> contacts = new ArrayList<>();
         contacts.add(contact);
 
-        // Build the user object using the builder pattern
-        return User.builder()
-                .firstName("Nikhil")
-                .lastName("Palve")
-                .username("nikhilpalve18")
-                .password("Nikhil@12345")
-                .contacts(contacts)
-                .build();
+        return User.builder().firstName("Nikhil").lastName("Palve").username("nikhilpalve18").
+                password("Nikhil@12345").contacts(contacts).build();
+    }
+
+    @Test
+    void testFindByUsername_FOUND() {
+        User userResult = userRepository.findByUsername(user.getUsername());
+        assertThat(user).isEqualTo(userResult);
+    }
+
+    @Test
+    void testFindByUsername_NotFOUND() {
+        String username = "nikhilpalve1889";
+        User expected = userRepository.findByUsername(username);
+        assertThat(expected).isNull();
     }
 
 }
